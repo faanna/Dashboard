2de4b71686469bd05463b1f352cd29f1b94e7dd5
faanna <sakurayu96@gmil.com>
2023-12-21 09:37:12 +0900

    Dashboard-ing

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let navigation = document.querySelector('.navigation');
+let toggle = document.querySelector('#btnCall');
+toggle.addEventListener('click', () => {
+	navigation.classList.toggle('on');
+});
